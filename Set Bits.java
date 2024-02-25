206cf40ef949b9d2193efdb9b106f3952613f8f6
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-02-25 19:13:50 +0530

    Create Set Bits.java
    Write a program to count the number of 1's in the binary representation of an integer.

---
+++ Set Bits.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int countSetBits(int n) {
+		// Write your code here.
+		int count=0;
+		String s="";
+		while(n>0){
+			s+=Integer.toString(n%2);
+			n/=2;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i)=='1') {
+				count++;
+			}
+		}
+		return count;
+	}
+}
