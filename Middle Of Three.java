3482f4914b4d351305097b1e5c392565510a0bbe
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-03-31 18:02:35 +0530

    You have been given three distinct integers ‘X’, ‘Y’ and ‘Z’. You need to find the number with a value in the middle.  For example :  X = 4, Y = 6, Z = 2 Here the element with value in the middle is 4, because 2 < 4 < 6. Note :  You need to try doing it using minimum comparisons.

--- Middle Of Three.java
+++ Middle Of Three.java
@@ -1,9 +1,37 @@
 import java.util.* ;
+
+
 import java.io.*; 
 public class Solution {
+
 	public static int middleOfThree(int x, int y, int z) {
-		int arr[]={x,y,z};
-		Arrays.sort(arr);
-		return arr[1];
+		if(x>y){
+			int temp=x;
+			x=y;
+			y=temp;
+		}
+		if(y>z){
+			int temp=y;
+			y=z;
+			z=temp;
+		}
+		if(x>y){
+			int temp=x;
+			x=y;
+			y=temp;
+		}
+		return y;
 	}
 }
+
+//A simpler way to do this code but will nit follow the minimum comparision rule
+
+// import java.util.* ;
+// import java.io.*; 
+// public class Solution {
+// 	public static int middleOfThree(int x, int y, int z) {
+// 		int arr[]={x,y,z};
+// 		Arrays.sort(arr);
+// 		return arr[1];
+// 	}
+// }
